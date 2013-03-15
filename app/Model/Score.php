cf1ca70ff2aa645bbba7a0e41affa723c248cc31
Stijn Ruts <stijn.ruts@adsl20.be>
2013-03-15 11:34:30 +0100

    Dynamische validatie voor scores

--- app/Model/Score.php
+++ app/Model/Score.php
@@ -15,4 +15,17 @@ class Score extends AppModel {
 	);
 
 	public $belongsTo = array('Point', 'User', 'Contestant', 'Round');
+
+	public function beforeValidate($options = array()){
+		$Score = ClassRegistry::init('Score');
+		$Score->id = $this->data['Score']['id'];
+		$score = $Score->read();
+		$max = $score['Point']['max'];
+		$this->validator()->getField('score')->setRule('max', array(
+			'rule' => array('comparison', '<=', $max),
+			'message' => 'Dit getal kan maximaal '.$max.' zijn'
+		));
+
+		//return true;
+	}
 }
