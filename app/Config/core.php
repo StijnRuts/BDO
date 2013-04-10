714b0fb2e2c386e3dddd2a1ca5220884f5badb07
Stijn Ruts <stijn.ruts@adsl20.be>
2013-04-10 15:02:59 +0200

    Revert "Always debug"
    This reverts commit b40016a066faa1824e698d781ff20f75be65c0d9.

--- app/Config/core.php
+++ app/Config/core.php
@@ -32,7 +32,11 @@
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
  */
-	Configure::write('debug', 2);
+	if( isset($_SERVER['SERVER_ADDR']) && $_SERVER['SERVER_ADDR']=='127.0.0.1' ){
+		Configure::write('debug', 2);
+	}else{
+		Configure::write('debug', 0);
+	}
 
 /**
  * Configure the Error handler used to handle errors for your application.  By default
