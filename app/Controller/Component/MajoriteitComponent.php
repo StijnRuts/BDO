191f7e7362dbd39a0524b5cf3891dd7cce1a5bf7
Stijn Ruts <stijn.ruts@adsl20.be>
2014-02-16 22:16:55 +0100

    bij dubbele meerderheid, ken plaats toe aan grootste meerderheid

--- app/Controller/Component/MajoriteitComponent.php
+++ app/Controller/Component/MajoriteitComponent.php
@@ -2,20 +2,20 @@
 class MajoriteitComponent extends Component {
 	public function getMajoriteit($contestants, $users) {
 
-		// bepaal plaats
+		// bepaal juryplaatsen
 		foreach($users as $user) {
 			$scores = array();
-			foreach ($contestants as &$contestant){
+			foreach ($contestants as $contestant){
 				array_push($scores, $contestant['scores']['scores'][$user['id']]['total']);
 			}
 			sort($scores);
 			$scores = array_reverse($scores);
 			foreach ($contestants as &$contestant) {
 				$contestant['places'][$user['id']] = array_search($contestant['scores']['scores'][$user['id']]['total'], $scores) + 1;
-			}
+			} unset($contestant);
 		}
 
-		// tel plaatsen
+		// tel juryplaatsen
 		foreach ($contestants as &$contestant) {
 			$placecount = array_count_values($contestant['places']);
 			$cumulative = 0;
@@ -28,46 +28,53 @@ class MajoriteitComponent extends Component {
 				$sum += $i * $plaatsing;
 				$contestant['plaatsing'][$i]['sum'] = $sum;
 			}
-			$contestant['assigned'] = false;
-		}
+			$contestant['place'] = false;
+		}  unset($contestant);
 
 		// ken plaatsen toe
 		$maj = ceil(count($users) / 2); // aantal stemmen nodig voor majoriteit
 		$assignplace = 1; // toe te kennen plaats
 		$maxplace = count($contestants); // maximum toe te kennen plaats
-		$placement = array(); // toegekende plaatsen
-		for ($i=1; $i<=count($contestants); $i++) {
+		$column = 1;
+		while ($column <= count($contestants)) {
 			$thisplace = array();
-			foreach ($contestants as &$contestant) {
-				if($contestant['plaatsing'][$i]['cumulative'] >= $maj){ // heeft majoriteit ?
-					if(!$contestant['assigned']){
-						array_push($thisplace, $contestant);
-					}
+			// wie heeft majoriteit ?
+			foreach ($contestants as $i => $contestant) {
+				if($contestant['plaatsing'][$column]['cumulative'] >= $maj  &&  !$contestant['place']){
+					array_push($thisplace, $i);
 				}
-				if( count($thisplace)==0 ) { // er is geen majoriteit -> kijk naar volgende kolom
-					// ???
-				} elseif( count($thisplace)==1 ) { // er is majoriteit -> ken plaats toe
-					$placement[$assignplace] = $contestant['id'];
-					$contestant['assigned'] = true;
+			}
+			if( count($thisplace)==0 ) { // er is geen majoriteit -> kijk naar volgende kolom
+				$column++;
+			} elseif( count($thisplace)==1 ) { // er is majoriteit -> ken plaats toe
+				$i = array_shift($thisplace);
+				$contestants[$i]['place'] = $assignplace;
+echo "rule1: place $assignplace goes to ".$contestants[$i]['startnr']."<br/>";
+				$assignplace++;
+				$column++;
+			} else { // er is dubbele majoriteit -> grootste cumulative
+				$max = 0;
+				foreach($thisplace as $i) $max = max($max, $contestants[$i]['plaatsing'][$column]['cumulative']);
+				$thisplace = array_filter($thisplace, function($i) use ($contestants, $column, $max){
+					return $contestants[$i]['plaatsing'][$column]['cumulative'] >= $max;
+				});
+				if( count($thisplace)==1 ) { // er is grootste cumulative -> ken plaats toe
+					$i = array_shift($thisplace);
+					$contestants[$i]['place'] = $assignplace;
+echo "rule2: place $assignplace goes to ".$contestants[$i]['startnr']."<br/>";
 					$assignplace++;
-				} elseif( count($thisplace)>1 ) { // er is dubbele majoriteit
-					// grootste cumulative							$contestant['plaatsing'][$i]['cumulative']
-					// bij gelijk -> laagste sum					$contestant['plaatsing'][$i]['sum']
+				} else { // er is geen grootste cumulative -> laagste sum
+					//$contestant['plaatsing'][$column]['sum']
 					// nog steeds gelijk -> kijk naar cummulative/sum van volgende plaats
 					// nog steeds gelijk -> deel plaats
 					//  * neem gemiddelde plaats van alle gelijke deelnemers
 					//  * rond af naar boven
 					//  * niet toegekende plaatsen vervallen
+					throw new Exception("Error", 1);
+					
 				}
 			}
 		}
-		foreach ($contestants as &$contestant) {
-			foreach ($placement as $p=>$c) {
-				if($contestant['id'] == $c) $contestant['place'] = $p;
-			}
-		}
-
-		// foreach ($placement as $p=>$c) echo $p . " -> " . $c['name'] . "<br />";
 
 		return $contestants;
 	}
