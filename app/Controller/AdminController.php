66de6ded78ab97211cb3078d3bf4c5bbb6559baf
Stijn Ruts <github@stijnruts.be>
2018-01-02 18:46:22 +0100

    Add debug function to generate random scores

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -33,5 +33,42 @@ class AdminController extends AppController {
 		}
 		$this->redirect( $this->referer()=="/" ? array('action'=>'index') : $this->referer() );
 	}
+
+	public function setRandomScores($contestId)
+	{
+		if (Configure::read('debug') < 2) {
+			echo "Only available in debug mode";
+			exit;
+		}
+		$this->loadModel('Contest');
+		$this->loadModel('Round');
+		$this->loadModel('Score');
+		if (!$this->Contest->exists($contestId)) {
+			throw new NotFoundException();
+		}
+		$contest = $this->Contest->findById($contestId);
+		$scores = array();
+		foreach ($contest['Point'] as $point) {
+			foreach ($contest['Round'] as $round) {
+				$round = $this->Round->findById($round['id']);
+				foreach ($round['Contestant'] as $contestant) {
+					foreach ($contest['User'] as $user) {
+						$scores[] = array(
+							'point_id' => $point['id'],
+							'user_id' => $user['id'],
+							'contestant_id' => $contestant['id'],
+							'round_id' => $round['Round']['id'],
+							'score' => rand(0, (int)$point['max']),
+						);
+					}
+				}
+			}
+		}
+		$roundIds = Set::extract('/Round/id', $contest);
+		$this->Score->deleteAll(array('Score.round_id' => $roundIds));
+		$this->Score->saveAll($scores, array('validate' => false));
+		echo "Done";
+		$this->redirect(array('action'=>'index'));
+	}
 }
 ?>
